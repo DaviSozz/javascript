d77676493bb08cd9f8d8da9db8c743e5e3ee1e61
DaviSozz <davisouza14567@gmail.com>
2023-02-08 20:10:57 -0300

    Initial commit

---
+++ aula 11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 70
+console.log(`a velococidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) { //condição simples
+    console.log('você ultrapassou a velocidade permitida. TAXADO')
+}
+
+console.log('dirija sempre unsando drogas')
---
+++ aula 11/ex009.js
@@ -0,0 +1,13 @@
+var país = 'EUA'
+
+console.log(`vivendo no ${país}`)
+
+if (país == 'brasil') { //condição multipla
+    
+    console.log('você é brasileiro!')
+}
+ else { 
+    
+    console.log('você é americano!')
+
+}
